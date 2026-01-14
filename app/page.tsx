6b406ecf9d0761d0dd0abe6015c79098b282d1fb
pinaki-GH <pinaki4frndz@gmail.com>
2026-01-14 11:36:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,6 @@ const getContrastingTextColor = (bgColor: string) => {
   const r = parseInt(hex.substring(0, 2), 16);
   const g = parseInt(hex.substring(2, 4), 16);
   const b = parseInt(hex.substring(4, 6), 16);
-
   const brightness = (r * 299 + g * 587 + b * 114) / 1000;
   return brightness > 160 ? "#000000" : "#FFFFFF";
 };
